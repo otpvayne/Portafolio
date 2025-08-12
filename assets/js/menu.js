284a25f440b102b12495ff7ee378c539b435878d
Diego Alejandro Medina Martinez <diego2303medinam@gmail.com>
2025-08-12 14:43:36 -0500

    tools

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -234,3 +234,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const observeredOnce = new WeakSet();
   items.forEach(it=>io.observe(it));
 })();
+document.querySelectorAll(".tab-button").forEach(btn => {
+  btn.addEventListener("click", () => {
+    document.querySelectorAll(".tab-button").forEach(b => b.classList.remove("active"));
+    document.querySelectorAll(".tab-content").forEach(tc => tc.classList.remove("active"));
+
+    btn.classList.add("active");
+    document.getElementById(btn.dataset.tab).classList.add("active");
+  });
+});
